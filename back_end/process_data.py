9ab4527bfd19bb3dfecd230fe5393be84d440d6d
Katie Arriaga Maldonado <katiearriagam@users.noreply.github.com>
2024-12-03 19:19:21 -0800

    Add Ruff for linting

--- back_end/process_data.py
+++ back_end/process_data.py
@@ -1,7 +1,9 @@
 import json
-import sys, os
 import logging
+import os
+import sys
 from typing import Dict
+
 import requests
 from dotenv import dotenv_values
 
@@ -9,10 +11,11 @@ sys.path.append(os.path.join(sys.path[0], '..'))
 # sys.path.append(os.path.join(sys.path[0], '../..'))
 
 from back_end.open_ai_processor import OpenAIProcessor
-from db_storage import get_conn
-from config import auto_config as config2
 
-config = dotenv_values(".env")
+from config import auto_config as config2
+from db_storage import get_conn
+
+config = dotenv_values('.env')
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 logger = logging.getLogger('cse580-process-data')
@@ -28,61 +31,58 @@ def query_news_api(name):
     NEWS_API_KEY = config['NEWS_API_KEY']
     logger.info('-- Fetching news API')
     # call API
-    res = requests.post(NEWS_API_ENDPOINT, headers={
-        'Content-Type': 'application/json'
-    }, data=json.dumps({
-        "query": {
-            "$query": {
-                "$and": [
-                {
-                    "keyword": name
+    res = requests.post(
+        NEWS_API_ENDPOINT,
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps(
+            {
+                'query': {
+                    '$query': {
+                        '$and': [
+                            {'keyword': name},
+                            {'lang': 'spa'},
+                            {'sourceLocationUri': 'http://en.wikipedia.org/wiki/Mexico'},
+                        ]
+                    },
+                    '$filter': {'forceMaxDataTimeWindow': '31'},
                 },
-                {
-                    "lang": "spa"
-                },
-                {
-                    "sourceLocationUri": "http://en.wikipedia.org/wiki/Mexico"
-                },
-                ]
-            },
-            "$filter": {
-                "forceMaxDataTimeWindow": "31"
+                'resultType': 'articles',
+                'articlesSortBy': 'date',
+                'articlesCount': 20,
+                'includeArticleLocation': True,
+                'includeArticleOriginalArticle': True,
+                'apiKey': NEWS_API_KEY,
             }
-        },
-        "resultType": "articles",
-        "articlesSortBy": "date",
-        "articlesCount": 20,
-        "includeArticleLocation": True,
-        "includeArticleOriginalArticle": True,
-        "apiKey": NEWS_API_KEY
-    }))
+        ),
+    )
     logger.debug(res.text)
     res.raise_for_status()
     logger.info(f'-- {res.status_code}')
     result = res.json()
-    articles = result["articles"]["results"]
+    articles = result['articles']['results']
     articles_processed = []
     for article in articles:
         article_metadata = {
-            "id": article["uri"], 
-            "name": name,
-            "source": article["source"]["title"],
-            "body": article["body"],
-            "title": article["title"],
-            "publication_date": article["dateTimePub"]
+            'id': article['uri'],
+            'name': name,
+            'source': article['source']['title'],
+            'body': article['body'],
+            'title': article['title'],
+            'publication_date': article['dateTimePub'],
         }
         articles_processed.append(article_metadata)
     logger.info(f'-- Fetched {len(articles_processed)} articles.')
     return articles_processed
+
 
 # Returns an array with the articles that are not already stored
 def filter_existing_articles(articles):
     # connect to the database
     acs = config['AZURE_SQL_CONNECTIONSTRING']
     db_articles = set()
-    get_articles_query = "SELECT id FROM articles"
+    get_articles_query = 'SELECT id FROM articles'
     # store the id of all the newly fetched articles
-    new_articles = set([x["id"] for x in articles])
+    new_articles = set([x['id'] for x in articles])
     with get_conn(acs) as conn:
         cursor = conn.cursor()
         # get all the articles form the articles table
@@ -93,8 +93,9 @@ def filter_existing_articles(articles):
     # get the set of all the articles fetched that do not currently
     # exist in the database (the net new articles)
     net_new_articles_id = new_articles - db_articles
-    # return the net new articles   
-    return [article for article in articles if article["id"] in net_new_articles_id]
+    # return the net new articles
+    return [article for article in articles if article['id'] in net_new_articles_id]
+
 
 # Store news API
 def store_news_articles(articles):
@@ -103,12 +104,22 @@ def store_news_articles(articles):
     net_new_articles = filter_existing_articles(articles)
     acs = config['AZURE_SQL_CONNECTIONSTRING']
     # store the net new articles in the database
-    insert_article_query = "insert into articles(id, victim_name, title, publication_date, source, body) values (?, ?, ?, ?, ?, ?);"
+    insert_article_query = (
+        'insert into articles(id, victim_name, title, publication_date, source, body) values (?, ?, ?, ?, ?, ?);'
+    )
     with get_conn(acs) as conn:
         cursor = conn.cursor()
         for article in net_new_articles:
             # create a new entry in the DB for a new article
-            cursor.execute(insert_article_query, article["id"], article["name"], article["title"], article["publication_date"], article["source"], article["body"])
+            cursor.execute(
+                insert_article_query,
+                article['id'],
+                article['name'],
+                article['title'],
+                article['publication_date'],
+                article['source'],
+                article['body'],
+            )
         conn.commit()
     logger.info(f'-- Inserted {len(net_new_articles)} rows to the articles table.')
 
@@ -119,15 +130,16 @@ def fetch_articles_from_db(name):
     # query the DB to retrieve the news article
     acs = config['AZURE_SQL_CONNECTIONSTRING']
     db_articles = []
-    select_articles_query = "SELECT title, body, publication_date FROM articles WHERE victim_name = ?;"
+    select_articles_query = 'SELECT title, body, publication_date FROM articles WHERE victim_name = ?;'
     with get_conn(acs) as conn:
         cursor = conn.cursor()
         # create a new entry in the DB for a new article
         cursor.execute(select_articles_query, name)
         for row in cursor.fetchall():
-            db_articles.append({"title": row[0], "body": row[1], "date_published": row[2].isoformat()})
+            db_articles.append({'title': row[0], 'body': row[1], 'date_published': row[2].isoformat()})
     logger.info(f'-- Found {len(db_articles)} articles in the database.')
     return json.dumps(db_articles, ensure_ascii=False)
+
 
 # Extract info with GPT
 def process_data(articles) -> Dict | None:
@@ -135,10 +147,10 @@ def process_data(articles) -> Dict | None:
     open_ai_processor = OpenAIProcessor()
     try:
         # open the file that contains the prompt
-        with open("coding_prompt.txt", "r") as f:
+        with open('coding_prompt.txt', 'r') as f:
             # read the entire content of the file as a string
             prompt = f.read()
-            logger.info(f'-- Calling Open AI to summarize the case.')
+            logger.info('-- Calling Open AI to summarize the case.')
             case_summary = open_ai_processor.summarize_case_info_from_articles(prompt, articles)
             try:
                 case_json = json.loads(case_summary)
@@ -153,15 +165,16 @@ def process_data(articles) -> Dict | None:
 def check_existing_case(case_metadata):
     logger.info('Checking for matching existing case in the database')
     acs = config['AZURE_SQL_CONNECTIONSTRING']
-    check_query = "SELECT id FROM cases WHERE victim_name LIKE ? AND missing_date = ?;"
+    check_query = 'SELECT id FROM cases WHERE victim_name LIKE ? AND missing_date = ?;'
     with get_conn(acs) as conn:
         cursor = conn.cursor()
         logger.info(f'Case metadata we are checking against {case_metadata}')
-        cursor.execute(check_query, f"%{case_metadata["victim_name"]}%", case_metadata["date_last_seen"])
+        cursor.execute(check_query, f"%{case_metadata["victim_name"]}%", case_metadata['date_last_seen'])
         existing_entry = cursor.fetchone()
         logger.info(f'Finished checking for existing entry and found {existing_entry}')
         return existing_entry[0] if existing_entry else None
-    
+
+
 # Store extracted info to DB
 def store_case_summary(case_metadata):
     logger.info('Storing extracted Open AI info')
@@ -175,39 +188,40 @@ def store_case_summary(case_metadata):
         if existing_entry_id is None:
             logger.info('-- Creating a new row')
             # insert query using parameterized SQL
-            insert_query = f"INSERT INTO cases (victim_name, missing_date, case_location, case_coordinates_latitude, case_coordinates_longitude, relationship_with_aggressor, case_status, victim_outcome, summary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);"
+            insert_query = f'INSERT INTO cases (victim_name, missing_date, case_location, case_coordinates_latitude, case_coordinates_longitude, relationship_with_aggressor, case_status, victim_outcome, summary) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);'
             # create a new entry in the DB for a new case
             cursor.execute(
                 insert_query,
-                case_metadata["victim_name"],
-                case_metadata["date_last_seen"],
-                case_metadata["location"],
-                case_metadata["coordinates"]["lat"],
-                case_metadata["coordinates"]["long"],
-                case_metadata["relationship_with_aggressor"],
-                case_metadata["status"],
-                case_metadata["victim_status"],
-                case_metadata["summary"]
+                case_metadata['victim_name'],
+                case_metadata['date_last_seen'],
+                case_metadata['location'],
+                case_metadata['coordinates']['lat'],
+                case_metadata['coordinates']['long'],
+                case_metadata['relationship_with_aggressor'],
+                case_metadata['status'],
+                case_metadata['victim_status'],
+                case_metadata['summary'],
             )
         else:
             logger.info('-- Updating existing row')
             # update query using parameterized SQL
-            update_query = "UPDATE cases SET victim_name = ?, missing_date = ?, case_location = ?, case_coordinates_latitude = ?, case_coordinates_longitude = ?, relationship_with_aggressor = ?, case_status = ?, victim_outcome = ?, summary = ? WHERE id = ?;"
+            update_query = 'UPDATE cases SET victim_name = ?, missing_date = ?, case_location = ?, case_coordinates_latitude = ?, case_coordinates_longitude = ?, relationship_with_aggressor = ?, case_status = ?, victim_outcome = ?, summary = ? WHERE id = ?;'
             # update the row for the existing case entry
             cursor.execute(
                 update_query,
-                case_metadata["victim_name"],
-                case_metadata["date_last_seen"],
-                case_metadata["location"],
-                case_metadata["coordinates"]["lat"],
-                case_metadata["coordinates"]["long"],
-                case_metadata["relationship_with_aggressor"],
-                case_metadata["status"],
-                case_metadata["victim_status"],
-                case_metadata["summary"],
-                existing_entry_id
+                case_metadata['victim_name'],
+                case_metadata['date_last_seen'],
+                case_metadata['location'],
+                case_metadata['coordinates']['lat'],
+                case_metadata['coordinates']['long'],
+                case_metadata['relationship_with_aggressor'],
+                case_metadata['status'],
+                case_metadata['victim_status'],
+                case_metadata['summary'],
+                existing_entry_id,
             )
         conn.commit()
+
 
 def process_name(name):
     logger.info(f'Processing {name}')
@@ -217,16 +231,18 @@ def process_name(name):
     case_summary = process_data(articles_in_db)
     if case_summary is not None:
         store_case_summary(case_summary)
-    
-    
+
+
 def run_pipeline():
     LIST_OF_NAMES = ['Bertha Guadalupe Cipriano']
     for name in LIST_OF_NAMES:
         process_name(name)
 
+
 def main():
     run_pipeline()
     logger.info('Done')
+
 
 if __name__ == '__main__':
     main()
