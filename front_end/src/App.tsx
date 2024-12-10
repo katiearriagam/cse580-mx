5e3cef97a8d87dffe628b228d2709e79000e8782
Katie Arriaga Maldonado <katiearriagam@users.noreply.github.com>
2024-12-09 19:51:45 -0800

    About section

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import MapPage from './pages/MapPage';
-import './styles/MapStyles.css';
+import React from "react";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MapPage from "./pages/MapPage";
+import "./styles/MapStyles.css";
+import AboutPage from "./pages/About";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div className="app-container">
-        <nav className="nav-bar">
-          <div className="nav-content">
-            <div className="nav-wrapper">
-              <div className="nav-links">
-                <Link to="/" className="nav-link">Home</Link>
-                <Link to="/map" className="nav-link">View Map</Link>
+    <FluentProvider theme={webLightTheme}>
+      <Router>
+        <div className="app-container">
+          <nav className="nav-bar">
+            <div className="nav-content">
+              <div className="nav-wrapper">
+                <div className="nav-links">
+                  <Link to="/" className="nav-link">
+                    Home
+                  </Link>
+                  <Link to="/about" className="nav-link">
+                    About
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </nav>
+          </nav>
 
-        <Routes>
-          <Route path="/" element={
-            <div className="home-content">
-              <h1 className="home-title">Welcome to the Map Application</h1>
-              <p className="home-description">Click "View Map" to see the interactive map.</p>
-            </div>
-          } />
-          <Route path="/map" element={<MapPage />} />
-        </Routes>
-      </div>
-    </Router>
+          <Routes>
+            <Route path="/" element={<MapPage />} />
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+        </div>
+      </Router>
+    </FluentProvider>
   );
 };
 
